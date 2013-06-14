08f12a033f296b879829a350c531a16314a5c78a
Juho Vepsalainen <bebraw@gmail.com>
2013-06-14 23:10:42 +0300

    Parse inner configuration properly

--- demo/index.js
+++ demo/index.js
@@ -4,10 +4,10 @@ var parseEnv = require('../lib/parse-env');
 var tpl = require('./config.template');
 
 // the configuration file is optional and doesn't have to exist
-var conf = {};
+var config = {};
 
 try {
-    conf = require('./config');
+    config = require('./config');
 }
 catch(e) {}
 
@@ -15,4 +15,4 @@ catch(e) {}
 // module.exports = parseEnv(process.env, tpl, conf);
 
 // now i'm just printing out the output
-console.log(parseEnv(process.env, tpl, conf));
+console.log(parseEnv(process.env, tpl, config));
